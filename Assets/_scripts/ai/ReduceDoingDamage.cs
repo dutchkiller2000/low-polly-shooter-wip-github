e7f86667dd5069fd693f7ebaff0e363fd2baff6a
dutchkiller2000 <markdelput@Kpnmail.nl>
2017-04-10 21:00:14 +0200

    wip| reduce damage if zombie get hit in the arms

---
+++ Assets/_scripts/ai/ReduceDoingDamage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ReduceDoingDamage : MonoBehaviour
+{
+    int procent;
+    int counter;
+    public int timesReduceSpeed;
+    [HeaderAttribute("the reducement of the damage in %!!!")]
+    public int damageIfShot;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "bullet")
+        {
+            Debug.Log("damage reduced");
+
+            procent = 100 - damageIfShot;
+            counter = counter + 1;
+            if (counter >= timesReduceSpeed)
+            {
+                GetComponentInChildren<zombieDamage>().damage = GetComponentInChildren<zombieDamage>().damage / 100 * procent;
+				//ERROR: cant find component because its cearching in child but this gm is already a child so it doent gave child
+            }
+        }
+    }
+}
