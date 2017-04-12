5adfa5e0ee5e173f8bf07e70d3c381e4651cbd6a
dutchkiller2000 <markdelput@Kpnmail.nl>
2017-04-12 11:05:19 +0200

    added ui inventory part and rebuild game

--- Assets/_scripts/money/store/StoreManager.cs
+++ Assets/_scripts/money/store/StoreManager.cs
@@ -1,24 +1,35 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class StoreManager : MonoBehaviour {
-	public KeyCode openStore;
-	bool StoreOpen = false;
-	void Start () {
-		
-	}
-	
-	void OnTriggerStay (Collider coll) {
-		Debug.Log("collider works");
-		if(coll.gameObject.tag == "Player"){
-			//press e to open store tab
-			Debug.Log("player can open store if press: " + openStore );
-			if(Input.GetKeyDown(openStore)){
-				Debug.Log("store open");
-				//pause game 
-				// store gm enabled
-				StoreOpen=true;
-			}
-		}
-	}
+public class StoreManager : MonoBehaviour
+{
+    public KeyCode openStore;
+    [HideInInspector]
+	public bool StoreOpen = false;
+    void Start()
+    {
+
+    }
+
+    void OnTriggerStay(Collider coll)
+    {
+        Debug.Log("collider works");
+        if (coll.gameObject.tag == "Player")
+        {
+            //press e to open store tab
+            Debug.Log("player can open store if press: " + openStore);
+            if (Input.GetKeyDown(openStore))
+            {
+
+                StoreOpen = true;
+            }
+        }
+        if (StoreOpen)
+        {
+            Debug.Log("store open");
+            //pause game 
+            // store gm enabled
+			//ispause = true
+        }
+    }
 }
